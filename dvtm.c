vkochan/evtm

commit f7b6a6de3a0402e266c5db91c2e7302a79f5a2c4

allow to refresh title of minimized window
It allows to dynamically update status via client's title
of minimized window.

Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

dvtm.c
@@ -508,7 +508,7 @@ draw(Client *c) {
 		redrawwin(c->window);
 		draw_content(c);
 	}
-	if (!isarrange(fullscreen) || sel == c)
+	if (!isarrange(fullscreen))
 		draw_border(c);
 	wnoutrefresh(c->window);
 }
@@ -725,7 +725,7 @@ term_title_handler(Vt *term, const char *title) {
 	c->title[title ? sizeof(c->title) - 1 : 0] = '\0';
 	c->sync_title = false;
 	settitle(c);
-	if (!isarrange(fullscreen) || sel == c)
+	if (!isarrange(fullscreen))
 		draw_border(c);
 	applycolorrules(c);
 }
@@ -2375,6 +2375,9 @@ main(int argc, char *argv[]) {
 					draw_content(c);
 					wnoutrefresh(c->window);
 				}
+			} else if (isvisible(c) && c->minimized) {
+				draw_border(c);
+				wnoutrefresh(c->window);
 			}
 		}
 

commit e96b3b040d4783fba4822a7dacae3edc7216df55

dvtm: draw: fix broken window bar is showed with 1 window
Fixes: f7b6a6de3a04 ("allow to refresh title of minimized window")
Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

dvtm.c
@@ -518,7 +518,7 @@ draw(Client *c) {
 		redrawwin(c->window);
 		draw_content(c);
 	}
-	if (!isarrange(fullscreen))
+	if (!isarrange(fullscreen) || c == sel)
 		draw_border(c);
 	wnoutrefresh(c->window);
 }