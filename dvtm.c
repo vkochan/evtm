vkochan/evtm

commit f7b6a6de3a0402e266c5db91c2e7302a79f5a2c4

allow to refresh title of minimized window
It allows to dynamically update status via client's title
of minimized window.

Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

dvtm.c
@@ -508,7 +508,7 @@ draw(Client *c) {
 		redrawwin(c->window);
 		draw_content(c);
 	}
-	if (!isarrange(fullscreen) || sel == c)
+	if (!isarrange(fullscreen))
 		draw_border(c);
 	wnoutrefresh(c->window);
 }
@@ -725,7 +725,7 @@ term_title_handler(Vt *term, const char *title) {
 	c->title[title ? sizeof(c->title) - 1 : 0] = '\0';
 	c->sync_title = false;
 	settitle(c);
-	if (!isarrange(fullscreen) || sel == c)
+	if (!isarrange(fullscreen))
 		draw_border(c);
 	applycolorrules(c);
 }
@@ -2375,6 +2375,9 @@ main(int argc, char *argv[]) {
 					draw_content(c);
 					wnoutrefresh(c->window);
 				}
+			} else if (isvisible(c) && c->minimized) {
+				draw_border(c);
+				wnoutrefresh(c->window);
 			}
 		}
 

commit a9293adf5399ba1e1bf7bd140c83c78d3a9f6553

fix minimized title appeared on fullscreen mode
Happens with min_align == MIN_ALIGN_VERT.

Fixes: f7b6a6de3a04 ("allow to refresh title of minimized window")
Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

dvtm.c
@@ -2652,7 +2652,8 @@ rescan:
 					draw_content(c);
 					wnoutrefresh(c->window);
 				}
-			} else if (isvisible(c) && c->minimized) {
+			} else if (!isarrange(fullscreen) && isvisible(c)
+					&& c->minimized) {
 				draw_border(c);
 				wnoutrefresh(c->window);
 			}